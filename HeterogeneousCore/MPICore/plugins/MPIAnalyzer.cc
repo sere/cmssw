9ec3ca5aa05987b3b2befdd31160f18a20cf90ae
Serena Ziviani <serena.ziviani@cern.ch>
2018-08-01 17:24:38 +0200

    Reformat with clang-format

--- HeterogeneousCore/MPICore/plugins/MPIAnalyzer.cc
+++ HeterogeneousCore/MPICore/plugins/MPIAnalyzer.cc
@@ -7,40 +7,43 @@
 #include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
 #include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/Event.h"
 #include "DataFormats/Common/interface/Handle.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-
-class MPIAnalyzer: public edm::stream::EDAnalyzer<> {
+class MPIAnalyzer : public edm::stream::EDAnalyzer<> {
 public:
-	explicit MPIAnalyzer(const edm::ParameterSet& config);
-	~MPIAnalyzer() override = default;
-	static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
-private:
-	void analyze(edm::Event const& event, edm::EventSetup const& setup) override;
+    explicit MPIAnalyzer(const edm::ParameterSet &config);
+    ~MPIAnalyzer() override = default;
+    static void fillDescriptions(edm::ConfigurationDescriptions &descriptions);
 
-	edm::EDGetTokenT<std::string> token_;
+private:
+    void analyze(edm::Event const &event,
+                 edm::EventSetup const &setup) override;
+
+    edm::EDGetTokenT<std::string> token_;
 };
 
-MPIAnalyzer::MPIAnalyzer(const edm::ParameterSet& config) :
-	token_(consumes<std::string>(config.getParameter<edm::InputTag>("msg_source"))) {
+MPIAnalyzer::MPIAnalyzer(const edm::ParameterSet &config)
+    : token_(consumes<std::string>(
+              config.getParameter<edm::InputTag>("msg_source"))) {}
+
+void MPIAnalyzer::analyze(edm::Event const &event,
+                          edm::EventSetup const &setup) {
+    edm::Handle<std::string> handle;
+    event.getByToken(token_, handle);
+    MPI_Send(static_cast<void const *>(handle.product()->data()),
+             handle.product()->size(), MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+    std::cout << "Buffer sent" << std::endl;
 }
 
-void MPIAnalyzer::analyze(edm::Event const& event, edm::EventSetup const& setup) {
-	edm::Handle<std::string> handle;
-	event.getByToken(token_, handle);
-	MPI_Send(static_cast<void const *>(handle.product()->data()), handle.product()->size(), MPI_CHAR, 1, 0, MPI_COMM_WORLD);
-	std::cout << "Buffer sent" << std::endl;
-}
-
-void
-MPIAnalyzer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-	edm::ParameterSetDescription desc;
-	desc.add<edm::InputTag>("msg_source", edm::InputTag());
-	descriptions.add("mpiAnalyzer", desc);
+void MPIAnalyzer::fillDescriptions(
+        edm::ConfigurationDescriptions &descriptions) {
+    edm::ParameterSetDescription desc;
+    desc.add<edm::InputTag>("msg_source", edm::InputTag());
+    descriptions.add("mpiAnalyzer", desc);
 }
 
 DEFINE_FWK_MODULE(MPIAnalyzer);
